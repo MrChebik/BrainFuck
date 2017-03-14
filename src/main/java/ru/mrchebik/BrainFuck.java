c3b13bd12e8859aa9868831a9a0ed657cfaf611a
mrchebik <mrchebik@yandex.ru>
2017-03-14 19:49:14 +0200

    slightly improved

--- src/main/java/ru/mrchebik/BrainFuck.java
+++ src/main/java/ru/mrchebik/BrainFuck.java
@@ -1,6 +1,7 @@
 package ru.mrchebik;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by mrchebik on 14.03.17.
@@ -10,11 +11,7 @@ public class BrainFuck {
     private static int k = 0;
 
     public static void main(String[] args) {
-        //String code = new Scanner(System.in).nextLine();
-
-        String code = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++" +
-                ".>+.+++++++..+++.>++.<<+++++++++++++++.>.+++." +
-                "------.--------.>+.>.";
+        String code = new Scanner(System.in).nextLine();
 
         brainfuck(code);
     }
@@ -26,27 +23,28 @@ public class BrainFuck {
             if (code.charAt(i) == '>') k++;      else
             if (code.charAt(i) == '<') k--;      else
             if (code.charAt(i) == '[') {
-                if (arr[k] < 0) {
-                    i = findMatchingBrace(i, code);
-                } else {
-                    String subcode = "";
-                    for (int h = ++i; h < code.length(); h++) {
-                        if (code.charAt(h) != ']') {
-                            subcode += code.charAt(h);
-                        } else {
-                            i = h;
+                String subcode = "";
+                int temp = 0;
+
+                for (i++; i < code.length(); i++) {
+                    if (code.charAt(i) == '[') {
+                        temp++;
+                    } else if (code.charAt(i) == ']') {
+                        if (temp == 0) {
                             break;
+                        } else {
+                            temp--;
                         }
                     }
+                    if (arr[k] <= 0) {
+                        subcode += code.charAt(i);
+                    }
+                }
 
+                if (arr[k] > 0) {
                     for (; arr[k] > 0;) {
                         brainfuck(subcode);
                     }
-                }
-            }                                    else
-            if (code.charAt(i) == ']') {
-                if (arr[k] != 0) {
-                    i = findNextBrace(i, code);
                 }
             }                                    else
             if (code.charAt(i) == '.') sout(k);  else
@@ -62,36 +60,7 @@ public class BrainFuck {
         }
     }
 
-    private static void sout(int k) {
-        System.out.print(arr[k]);
-    }
-
-    private static int findNextBrace(int i, String code) {
-        for (int j = i; j < code.length(); j++) {
-            if (code.charAt(j) == '[') {
-                return j;
-            }
-        }
-
-        return Integer.MAX_VALUE;
-    }
-
-    private static int findMatchingBrace(int i, String code) {
-        int temp = 0;
-        String subcode = "";
-
-        for (int j = i; j < code.length(); j++) {
-            if (code.charAt(j) == '[') {
-                temp++;
-            } else if (code.charAt(j) == ']') {
-                if (temp == 0) {
-                    return j;
-                } else {
-                    temp--;
-                }
-            }
-        }
-
-        return Integer.MAX_VALUE;
+    private static void sout(int i) {
+        System.out.print(arr[i]);
     }
 }
